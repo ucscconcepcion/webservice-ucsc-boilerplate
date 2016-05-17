4a983d37f573ee1a69f957e92786cb2de978e9ea
José Cabezas <jcabezas@bluk.cl>
2016-05-17 14:39:35 -0400

    update controller and model modulo

--- webservice/controllers/modulo.php
+++ webservice/controllers/modulo.php
@@ -1,1 +1,35 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
+require APPPATH.'/libraries/REST_Controller.php';
+
+/**
+ * Modulo Class Controller
+ *
+ * @package      WebService UCSC
+ * @subpackage   Nombre Modulo Webservice
+ * @category     Controller
+ * @author       Nombre Autor
+ * @link         https://ws.ucsc.cl/webservice/nombre_modulo/
+ */
+class Modulo extends REST_Controller {
+
+	/**
+	 * Clave generada por la Dirección de Servicios Informáticos UCSC
+	 *
+	 * @var    string
+	 */
+	protected $key = "";
+
+	/**
+	 * Construct model
+	 *
+	 * @param    void
+	 * @return    void
+	 */
+	public function __construct()
+	{
+		parent::__construct();
+
+		$this->load->model('modulo_model');
+	}
+}
--- webservice/models/modulo_model.php
+++ webservice/models/modulo_model.php
@@ -1,1 +1,39 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
+/**
+ * Informes Class Model
+ *
+ * @package      WebService UCSC
+ * @subpackage   Nombre Modulo Webservice
+ * @category     Model
+ * @author       Nombre Autor
+ * @link         https://ws.ucsc.cl/webservice/nombre_modulo/
+ */
+class Modulo_model extends CI_Model{
+
+  /**
+   * Configuración Base datos entregada por la Dirección de Servicios Informáticos UCSC
+   *
+   * @var    array
+   */
+   private $config = array();
+
+	/**
+	 * Construct model
+	 *
+	 * @param    void
+	 * @return    void
+	 */
+	public function __construct()
+	{
+		parent::__construct();
+
+		$this->config['hostname'] = "";
+		$this->config['username'] = "";
+		$this->config['password'] = "";
+		$this->config['database'] = "";
+		$this->config['dbdriver'] = "";
+		$this->db = $this->load->database($this->config);
+	}
+
+}
